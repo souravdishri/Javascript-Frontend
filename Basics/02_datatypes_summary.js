24ed614530a9b8597fd1dfd1d439c48b1edda1b3
Sourav Dishri <souravdishri@gmail.com>
2024-12-16 08:42:07 +0530

    add comment

--- Basics/02_datatypes_summary.js
+++ Basics/02_datatypes_summary.js
@@ -1,6 +1,8 @@
-//  Primitive
+//  Primitive (call by value)
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// Symbol (used to make any value unique (ex. Component - button))
+// BigInt (Scientific value/big values)
 
 const score = 100
 const scoreValue = 100.3
@@ -9,18 +11,19 @@ const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
 
-const id = Symbol('123')
+const id = Symbol('123')    //return type will be symbol
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id === anotherId);  //false
 
 // const bigNumber = 3456543576654356754n
 
 
 
-// Reference (Non primitive)
+// Non primitive (Call by Reference)
 
 // Array, Objects, Functions
+//  Return type will be object
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
