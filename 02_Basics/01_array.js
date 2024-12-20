3fa1af31c783665204c4d04489953fba892da15d
Sourav Dishri <souravdishri@gmail.com>
2024-12-20 19:42:40 +0530

    add desc

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -12,13 +12,15 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(7)
 // myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9)     //insert element to infront of the array (time consuming)
+// myArr.shift()        //delete element from infront of the array
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+//questionable methods
 
-// const newArr = myArr.join()
+// console.log(myArr.includes(9));  //check whether '9' is there or not (returns true/false)
+// console.log(myArr.indexOf(3));   //returns the index no. of '3'
+
+// const newArr = myArr.join()      //bind & convert it into 'string'
 
 // console.log(myArr);
 // console.log( newArr);
@@ -28,12 +30,37 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3)      //extract a portion of an array without modifying the original array.
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3) //To add, remove, or replace elements in an array and modifies the original array.
 console.log("C ", myArr);
-console.log(myn2);
+console.log(myn2);                  //returns new array with removed elements
+
+
+//Difference between slice & splice
+
+// (slice)
+// Effect: Does not change the original array
+// Purpose: Extracts elements into a new array
+// Returns:	New array with extracted elements	
+
+// Syntax: array.slice(startIndex, endIndex)
+// startIndex: The index to start the extraction (inclusive).
+// endIndex: The index to stop the extraction (exclusive).
+
+
+
+
+// (splice)
+// Effect: Changes the original array
+// Purpose: Adds, removes, or replaces elements
+// Returns: New array with removed elements (if any)
+
+// Syntax: array.splice(startIndex, deleteCount, item1, item2, ...)
+// startIndex: The index where changes will begin.
+// deleteCount: The number of elements to remove.
+// item1, item2, ...: Elements to add at the startIndex (optional).
