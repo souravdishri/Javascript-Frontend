084527bc6bca62770b973a1399f36451a2b02c95
Sourav Dishri <souravdishri@gmail.com>
2024-12-21 17:31:42 +0530

    add desc

--- 02_Basics/04_object.js
+++ 02_Basics/04_object.js
@@ -1,3 +1,5 @@
+// singleton        //made by constructor
+
 // const tinderUser = new Object()
 const tinderUser = {}
 
@@ -24,12 +26,14 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
+
+// we can do like this as well, '{}' and 'obj1, obj2' is the source
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
-
+// Array of objects
 const users = [
     {
         id: 1,
@@ -48,13 +52,22 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
+// find all 'keys' of the 'specified' object & returns it in a new 'array[]'
 // console.log(Object.keys(tinderUser));
+
+// find all 'values' of the 'specified' object & returns it in a new 'array[]'
 // console.log(Object.values(tinderUser));
+
+// we get 'array inside array', each (key,value) pairs are showed in array format
 // console.log(Object.entries(tinderUser));
 
+// checked if the object has 'isLoggedIn' type of property is present or not, & return will be in boolean (true/false)
+// use this to 'avoid crash', if it's 'not present' 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
+
+// Destructuring
 const course = {
     coursename: "js in hindi",
     price: "999",
@@ -63,6 +76,7 @@ const course = {
 
 // course.courseInstructor
 
+//object destructuring
 const {courseInstructor: instructor} = course
 
 // console.log(courseInstructor);
