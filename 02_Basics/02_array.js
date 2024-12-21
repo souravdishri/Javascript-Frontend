6eb48672ca2df17be34774522f32938cb7d67125
Sourav Dishri <souravdishri@gmail.com>
2024-12-21 08:54:58 +0530

    add desc

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -1,31 +1,33 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)      //array can take any type of data object, boolean as well
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros)   //combines two or more arrays and returns a new array without modifying any existing arrays
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [...marvel_heros, ...dc_heros]        //using spread operator
 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+//returns a new array with all the sub-array elements concatenated into it recursively upto the specified depth
+const real_another_array = another_array.flat(Infinity)     
 console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Sourav"))
-console.log(Array.from("Sourav"))
-console.log(Array.from({name: "sourav"})) // interesting
+console.log(Array.isArray([1,2]))       //checks if its array or not  
+console.log(Array.isArray('Sourav'))       //checks if its array or not  
+console.log(Array.from("Sourav"))       //convert to array, we can pass objects, strings as well
+console.log(Array.from({name: "sourav"})) // interesting, returns empty array as it can't convert
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));  //returns a new array from a set of elements
