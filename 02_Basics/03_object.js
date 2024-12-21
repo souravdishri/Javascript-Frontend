6f86ca5aa2996d2ca2039e4dc5f7abde9323ac53
Sourav Dishri <souravdishri@gmail.com>
2024-12-21 15:19:51 +0530

    add desc

--- 02_Basics/03_object.js
+++ 02_Basics/03_object.js
@@ -1,8 +1,9 @@
-// singleton
+// singleton        //made by constructor
 // Object.create
 
 // object literals
 
+//this is how we declare 'Symbol' ans used as key in object
 const mySym = Symbol("key1")
 
 
@@ -18,21 +19,27 @@ const JsUser = {
 }
 
 // console.log(JsUser.email)
-// console.log(JsUser["email"])
+
+// console.log(JsUser["email"])     //we should used this
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+
+// console.log(JsUser[mySym])       //we should use like this to use 'Symbol' as 'datatype'
+
 
 JsUser.email = "sourav@chatgpt.com"
-// Object.freeze(JsUser)
+// Object.freeze(JsUser)        //by the use of this values will not change/propagate
 JsUser.email = "sourav@microsoft.com"
 // console.log(JsUser);
 
+
+//we are adding functions inside object
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    console.log(`Hello JS user, ${this.name}`); //we have to use 'this' when we want reference to the 'same object'
 }
 
+console.log(JsUser.greeting);       //function not executed but get the reference
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
