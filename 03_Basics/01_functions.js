a90acc2797bbf0e8af043d0c5530882f1170923f
Sourav Dishri <souravdishri@gmail.com>
2024-12-22 12:07:37 +0530

    add desc

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -8,26 +8,29 @@ function sayMyName(){
     console.log("V");
 }
 
-// sayMyName()
+// sayMyName    //(reference)
+// sayMyName()  //(execute)
 
 // function addTwoNumbers(number1, number2){
 
 //     console.log(number1 + number2);
 // }
 
-function addTwoNumbers(number1, number2){
+function addTwoNumbers(number1, number2){   //this is called 'parameters'
 
     // let result = number1 + number2
     // return result
     return number1 + number2
+    //console.log("This will not executed");  //(this will not executed as it is after return)
+    
 }
 
-const result = addTwoNumbers(3, 5)
+const result = addTwoNumbers(3, 5)  //this is called 'arguments'
 
 // console.log("Result: ", result);
 
 
-function loginUserMessage(username = "sam"){
+function loginUserMessage(username = "sam"){    //default values
     if(!username){
         console.log("PLease enter a username");
         return
@@ -39,8 +42,8 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("sourav"))
 
 
-function calculateCartPrice(val1, val2, ...num1){
-    return num1
+function calculateCartPrice(val1, val2, ...num1){   //(rest/spread) operator
+    return num1     //returns [500, 2000]
 }
 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
@@ -50,16 +53,20 @@ const user = {
     prices: 199
 }
 
+//How to pass 'object' in function
 function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
-// handleObject(user)
+// handleObject(user)   //Mandatory object name
+
 handleObject({
     username: "sam",
     price: 399
 })
 
+
+//How to pass 'array' in function
 const myNewArray = [200, 400, 100, 600]
 
 function returnSecondValue(getArray){
@@ -67,4 +74,5 @@ function returnSecondValue(getArray){
 }
 
 // console.log(returnSecondValue(myNewArray));
+
 console.log(returnSecondValue([200, 400, 500, 1000]));
