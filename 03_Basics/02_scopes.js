1d45fefa8c030bd26cb09c79f8e505d46430df96
Sourav Dishri <souravdishri@gmail.com>
2024-12-22 14:57:37 +0530

    add desc

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -1,27 +1,29 @@
-//var c = 300
+// var c = 300   //Global scope
 let a = 300
 if (true) {
-    let a = 10
+    let a = 10      //Local scope
     const b = 20
     // console.log("INNER: ", a);
 
 }
 
 
-
 // console.log(a);
 // console.log(b);
 // console.log(c);
 
+// Global scope is different for both 'node environment' and 'browser environment'
 
 function one() {
     const username = "sourav"
 
     function two() {
         const website = "youtube"
-        console.log(username);
+        // A 'closure' in JavaScript is a function that can access the variables in its parent function's scope, 
+        // even after the parent function has returned
+        console.log(username);  //can access outer function variables, (for it, it is his global scope)
     }
-    // console.log(website);
+    // console.log(website);    //(can not access)
 
     two()
 
@@ -35,10 +37,10 @@ if (true) {
         const website = " youtube"
         // console.log(username + website);
     }
-    // console.log(website);
+    // console.log(website);    //(no access)
 }
 
-// console.log(username);
+// console.log(username);   //(no access)
 
 
 // ++++++++++++++++++ interesting ++++++++++++++++++
@@ -51,8 +53,8 @@ function addone(num) {
 }
 
 
-
-addTwo(5)
+//here we are holding in a variable, with declaration, so it show 'error'
+//addTwo(5)
 const addTwo = function (num) {
     return num + 2
 }
