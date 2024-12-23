c104dfbba4243e9085b20133012a0ea00c7ad3ee
Sourav Dishri <souravdishri@gmail.com>
2024-12-23 09:41:04 +0530

    add desc

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -5,6 +5,12 @@ const user = {
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
         console.log(this);
+        // show current context that is.
+        // {
+        //     username: "sourav",
+        //     price: 999,
+        //     welcomeMessage: [function: welcomeMessage] 
+        // }
     }
 
 }
@@ -13,14 +19,18 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this);
+
+// console.log(this);       //empty object that is '{}' will show on node environment
+// 'window {}' is the global object in the browser
+
 
 // function chai(){
 //     let username = "sourav"
-//     console.log(this.username);
+//     console.log(this.username);  //(we can't use like this in function as before we are using in object)
 // }
 
 // chai()
+
 
 // const chai = function () {
 //     let username = "sourav"
@@ -29,21 +39,22 @@ const user = {
 
 const chai =  () => {
     let username = "sourav"
-    console.log(this);
+    console.log(this.username); //output will be 'undefined'
+    console.log(this);          //output will be '{}' empty object
 }
 
 
 // chai()
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
+// const addTwo = (num1, num2) => {     //if we are using "{}" we must have to 'return' keyword
+//     return num1 + num2               //explicit return
 // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  num1 + num2      //implicit return
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+// const addTwo = (num1, num2) => ( num1 + num2 )   //if we are using "()" we don't need to write 'return' keyword
 
-const addTwo = (num1, num2) => ({username: "sourav"})
+const addTwo = (num1, num2) => ({username: "sourav"})   //return object
 
 
 console.log(addTwo(3, 4))
