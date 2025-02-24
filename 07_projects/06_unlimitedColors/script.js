17e0949fb0c19c4b567d06a396d505ee79370e27
Sourav Dishri <souravdishri@gmail.com>
2025-02-24 21:21:04 +0530

    bgChanger in every sec

---
+++ 07_projects/06_unlimitedColors/script.js
@@ -0,0 +1,31 @@
+//generate a random color
+
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    // Uses the random integer as an index to select a character from the hex string, range will be (0-15)
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let intervalId;
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;    //for edge-cases
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
+
