7d1e90ad1f8ac4aecf96427b49445ba7890f3244
Sourav Dishri <souravdishri@gmail.com>
2025-07-03 21:54:19 +0530

    fix typo in comment in one.js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -33,7 +33,7 @@ for (let index = 0; index < myArray.length; index++) {
 // for (let index = 1; index <= 20; index++) {
 //     if (index == 5) {
 //         console.log(`Detected 5`);
-//         break                     //w(hen it detected it comes out of this block as well as the outer block)
+//         break                     //(when it detected it comes out of this block as well as the outer block)
 //     }
 //     console.log(`Value of i is ${index}`);
 
