6534547e40f29201e3b5f1a10e05ada099034897
Sourav Dishri <souravdishri@gmail.com>
2025-07-22 22:19:45 +0530

    add User class with getter and setter for email and password properties

---
+++ 10_classes_and_oop/properties_get_set.js
@@ -0,0 +1,29 @@
+// Older syntax
+// we will use this when we don't want everyone to access properties directly 
+
+function User(email, password) {
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function () {
+            return this._email.toUpperCase()
+        },
+        set: function (value) {
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function () {
+            return this._password.toUpperCase()
+        },
+        set: function (value) {
+            this._password = value
+        }
+    })
+
+}
+
+const chai = new User("chai@chai.com", "chai")
+
+console.log(chai.email);
